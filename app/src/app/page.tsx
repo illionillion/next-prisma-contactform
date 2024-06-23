006ab0fcf4ede793950454cb0ae96f5f499c7cf1
yuto ichinose <yuto.0526.sano@gmail.com>
2024-06-23 20:03:58 +0900

    feat: 送信フォーム追加

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,95 +1,77 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import { Button, Center, Container, FormControl, Heading, Input, Textarea, VStack } from "@yamada-ui/react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export default function Home() {
+
+  const [name, setName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [content, setContent] = useState<string>('')
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+
+    const value = e.currentTarget.value
+
+    switch (e.currentTarget.name) {
+      case "name":
+        setName(value)
+        break;
+      case "email":
+        setEmail(value)
+        break;
+      case "content":
+        setContent(value)
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+      const response = await fetch('/api/inquiry', {
+        method: 'POST',
+        body: JSON.stringify({
+          name,
+          email,
+          content
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        }
+      })
+      const json = await response.json()
+      console.log(json);
+      if (response.status === 200) {
+        setName('')
+        setEmail('')
+        setContent('')
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Container m="auto" as={Center}>
+      <Heading>お問合せフォーム</Heading>
+      <form onSubmit={handleSubmit}>
+        <VStack>
+          <FormControl label="お名前" minW="md" isRequired>
+            <Input type="text" name="name" placeholder="名前" value={name} onChange={handleChange} />
+          </FormControl>
+          <FormControl label="メールアドレス" minW="md" isRequired>
+            <Input type="email" name="email" placeholder="メールアドレス" value={email} onChange={handleChange} />
+          </FormControl>
+          <FormControl label="内容" minW="md" isRequired>
+            <Textarea placeholder="内容" name="content" value={content} onChange={handleChange} />
+          </FormControl>
+          <Button type="submit">送信</Button>
+        </VStack>
+      </form>
+    </Container>
   );
 }
